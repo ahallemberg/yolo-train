663f78975fa4131b4ecc1067a80cb7fcaf4517a7
ahallemberg <ahallemberg@gmail.com>
2024-02-23 13:36:44 +0100

    Added some params for training

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-cp "${PROJECT_DIR}/.configs/params.train.default.yaml" "${PROJECT_DIR}/params/train.yaml"
-cp "${PROJECT_DIR}/.configs/params.tune.default.yaml" "${PROJECT_DIR}/params/tune.yaml"
+cp "${PROJECT_DIR}/.default/train.yaml" "${PROJECT_DIR}/params/train.yaml"
+cp "${PROJECT_DIR}/.default/tune.yaml" "${PROJECT_DIR}/params/tune.yaml"
+cp "${PROJECT_DIR}/.default/data.yaml" "${PROJECT_DIR}/datasets/data.yaml"
